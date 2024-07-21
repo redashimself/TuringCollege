87e2c6e14b73da52b1a000d6f39a8300d77c8d92
Redas Kriksciukaitis <redas.kriksciukaitis@gmail.com>
2024-07-21 17:51:30 +0300

    Add tests

--- A Chess Question/helpers_tests.py
+++ A Chess Question/helpers_tests.py
@@ -1,6 +1,8 @@
 from main import map_letter_to_board_column_coordinate
 from main import map_number_to_board_row_coordinate
-from main import get_piece_array_indices
+from main import place_piece
+from main import create_chess_board
+from main import process_piece_placement_input
 from column_coordinate import Column
 
 
@@ -15,7 +17,34 @@ def test_map_number_to_board_row_coordinate():
     assert map_number_to_board_row_coordinate("Knight a8") == 0
 
 
-def test_get_piece_array_indices():
-    assert get_piece_array_indices("Knight e1") == 7
-    assert get_piece_array_indices("Rook e4") == 4
-    assert get_piece_array_indices("Pawn a8") == 0
+def test_process_piece_placement_input():
+    row, col, piece = process_piece_placement_input("Knight e4")
+    assert row == 4
+    assert col == Column.e.value
+    assert piece == "Knight"
+
+    # Test bottom left
+    row, col, piece = process_piece_placement_input("Rook a1")
+    assert row == 7
+    assert col == Column.a.value
+    assert piece == "Rook"
+
+    # Test top right
+    row, col, piece = process_piece_placement_input("Queen h8")
+    assert row == 0
+    assert col == Column.h.value
+    assert piece == "Queen"
+
+
+def test_place_piece():
+    chess_board = create_chess_board()
+    place_piece(chess_board, 3, 4, "Knight", "White")
+    assert chess_board[3][4]['piece'] == "Knight"
+    assert chess_board[3][4]['color'] == "White"
+    assert chess_board[3][4]['position'] == "e5"
+
+    # Check if surrounded pieces unaffected
+    assert chess_board[3][3]['piece'] is None
+    assert chess_board[3][5]['piece'] is None
+    assert chess_board[2][4]['piece'] is None
+    assert chess_board[4][4]['piece'] is None
